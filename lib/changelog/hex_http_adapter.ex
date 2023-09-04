2c97b6ef0a04cd6a6057d2e75ffc259c1e882a7a
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2023-09-04 19:39:25 +0300

    Bump :req dependency version to 0.3

--- lib/changelog/hex_http_adapter.ex
+++ lib/changelog/hex_http_adapter.ex
@@ -4,11 +4,13 @@ defmodule Changelog.HexHTTPAdapter do
   @see https://github.com/hexpm/hex_core#configuration
   """
 
+  alias Req.Request
+
   @doc false
-  def request(method, uri, req_headers, _body, _adapter_config) do
-    resp =
-      Req.build(method, uri, headers: req_headers)
-      |> Req.run!()
+  def request(method, url, req_headers, _body, _adapter_config) do
+    {_req, resp} =
+      Request.new(method: method, url: url, headers: Map.to_list(req_headers))
+      |> Request.run_request()
 
     {:ok, {resp.status, Map.new(resp.headers), resp.body}}
   end
